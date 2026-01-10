8b0bc6ac4b323cc36c65e44ec20521a5f2eff7fa
rodrigocitadin <citadinrodrigo@gmail.com>
2026-01-10 16:51:39 -0300

    shard manager

---
+++ internal/repository/shard_manager.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"hash/fnv"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type ShardManager struct {
+	shards []*sql.DB // Slice of connections: [db0, db1, db2]
+}
+
+func NewShardManager(dsns []string) (*ShardManager, error) {
+	var conns []*sql.DB
+
+	for i, dsn := range dsns {
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			return nil, fmt.Errorf("failed to connect to the shard %d: %w", i, err)
+		}
+
+		if err := db.Ping(); err != nil {
+			return nil, fmt.Errorf("shard %d unreacheble: %w", i, err)
+		}
+
+		db.SetMaxOpenConns(10)
+		db.SetMaxIdleConns(5)
+
+		conns = append(conns, db)
+		log.Printf("connected to shard %d", i)
+	}
+
+	return &ShardManager{shards: conns}, nil
+}
+
+func (sm *ShardManager) GetShard(key string) *sql.DB {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	hashValue := h.Sum32()
+
+	shardIndex := hashValue % uint32(len(sm.shards))
+
+	return sm.shards[shardIndex]
+}
