c556d460731a8dc01fd958cdc17adf8b8a386ccc
rodrigocitadin <citadinrodrigo@gmail.com>
2026-01-13 20:08:26 -0300

    migration

---
+++ migrations/20260112173310_initial_migration.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id BIGSERIAL PRIMARY KEY,
+    shortcode VARCHAR(20) NOT NULL UNIQUE,
+    url TEXT NOT NULL,
+    accesses BIGINT NOT NULL DEFAULT 0 CHECK (accesses >= 0)
+);
