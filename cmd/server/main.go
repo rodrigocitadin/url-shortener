c72da6a85cd472310a85d88c9390eae3bbbd9bca
rodrigocitadin <citadinrodrigo@gmail.com>
2026-01-14 21:02:55 -0300

    middlewares to main server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rodrigocitadin/url-shortener/api"
+	"github.com/rodrigocitadin/url-shortener/api/handlers"
 	"github.com/rodrigocitadin/url-shortener/internal/repository"
 	"github.com/rodrigocitadin/url-shortener/internal/services"
 )
+
+var loggerConfig = middleware.RequestLoggerConfig{
+	LogStatus:    true,
+	LogURI:       true,
+	LogMethod:    true,
+	LogError:     true,
+	LogRequestID: true,
+	HandleError:  true,
+	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+		errStr := ""
+		if v.Error != nil {
+			errStr = v.Error.Error()
+		}
+
+		log.Printf("time=%s id=%s method=%s uri=%s status=%d error=%s\n",
+			v.StartTime.Format(time.RFC3339),
+			v.RequestID,
+			v.Method,
+			v.URI,
+			v.Status,
+			errStr,
+		)
+		return nil
+	},
+}
 
 func main() {
 	dsnsEnv := os.Getenv("SHARD_DSNS")
@@ -28,10 +57,15 @@ func main() {
 	serviceChain := api.ServiceChain{URLService: urlService}
 
 	//
-	// routes and API things
+	// routes and API
 	//
 
 	e := echo.New()
+	e.Use(middleware.RequestID())
+	e.Use(middleware.RequestLoggerWithConfig(loggerConfig))
+	e.Use(middleware.Recover())
+	e.HTTPErrorHandler = handlers.HTTPErrorHandler
+
 	api.Router(e, serviceChain)
 
 	e.Logger.Fatal(e.Start(":3030"))
